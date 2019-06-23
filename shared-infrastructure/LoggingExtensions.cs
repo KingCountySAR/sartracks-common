49f258755f73f990d68facf14b76aeb9420f43e0
Matt Cosand <matt@cosand.net>
2019-06-22 22:48:01 -0700

    Use Serilog for logging, write to console and rolling file.

--- shared-infrastructure/LoggingExtensions.cs
+++ shared-infrastructure/LoggingExtensions.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.IO;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace SarData.Logging
 {
   public static class LoggingExtensions
   {
-    public static ILoggingBuilder AddSarDataLogging(this ILoggingBuilder builder)
+    public static ILoggingBuilder AddSarDataLogging(this ILoggingBuilder builder, string contentRoot)
     {
+      Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.RollingFile(Path.Combine(contentRoot, "log-{Date}.txt"), restrictedToMinimumLevel: LogEventLevel.Information)
+        .CreateLogger();
+
       return builder
-        .AddConsole()
+        .AddSerilog()
         .AddApplicationInsights()
         .AddAzureWebAppDiagnostics();
     }
