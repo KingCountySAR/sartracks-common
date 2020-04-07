24117ecb12bda83e1bda939e16f010fac37778d8
Matt Cosand <matt@cosand.net>
2020-04-06 23:16:34 -0700

    Database API client - add GetMemberRequiredTraining

--- api-clients/Database/IDatabaseApi.cs
+++ api-clients/Database/IDatabaseApi.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Refit;
 using SarData.Common.Apis.Database.Training;
@@ -11,5 +10,8 @@ namespace SarData.Common.Apis.Database
   {
     [Post("/trainingrecords")]
     Task<TrainingRecord> CreateTrainingRecord([Body] TrainingRecord trainingRecord);
+
+    [Get("/members/{memberId}/requiredtraining")]
+    Task<List<TrainingStatus>> ListMemberRequiredTraining(Guid memberId);
   }
 }
---
+++ api-clients/Database/Training/TrainingStatus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SarData.Common.Apis.Database.Training
+{
+  public class TrainingStatus
+  {
+    public Guid Id { get; set; }
+    public DateTimeOffset? Expires { get; set; }
+    public ExpirationFlags Status { get; set; }
+    public DateTimeOffset? Completed { get; set; }
+    public NameIdPair Course { get; set; }
+
+    public string EventType { get; set; }
+    public Guid? EventId { get; set; }
+    public bool FromRule { get; set; }
+  }
+
+  [Flags]
+  public enum ExpirationFlags
+  {
+    Unknown = 0,
+    Bad = 1,
+    Missing = 3,
+    Expired = 5,
+    ToBeCompleted = 2,
+    NotNeeded = 8,
+    Complete = 16,
+    NotExpired = 32
+  }
+}
