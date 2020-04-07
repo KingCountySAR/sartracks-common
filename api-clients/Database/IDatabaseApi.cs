a7f1f50cd5e5561d46dee80db024675fd8f1dfd2
Matt Cosand <matt@cosand.net>
2020-04-06 18:58:33 -0700

    Database endpoint for submitting new training records

---
+++ api-clients/Database/IDatabaseApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+using SarData.Common.Apis.Database.Training;
+
+namespace SarData.Common.Apis.Database
+{
+  public interface IDatabaseApi
+  {
+    [Post("/trainingrecords")]
+    Task<TrainingRecord> CreateTrainingRecord([Body] TrainingRecord trainingRecord);
+  }
+}
